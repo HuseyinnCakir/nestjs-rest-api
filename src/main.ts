9d7b27b7f5cefb44598489b4ccd594141c04e896
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2025-02-21 23:41:17 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true, // Gelen requestlerde DTO'da tanımlanmayan property'lerin gelmesini engeller
+    transform: true, // Gelen requestlerde DTO'da tanımlanan property'lerin tip dönüşümünü yapar. type safety sağlar.
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
