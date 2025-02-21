6b30ceca8b4353f8db0bcac87e1538b6a905d415
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2025-02-21 23:41:39 +0300

    update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { getUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user-dto';
 
 @Controller('users')
 export class UsersController {
 
     @Get('/:id')
     public getUsers(
-        @Param('id') id?: number | undefined,
+        @Param() getUsersParamDto: getUsersParamDto,
         @Query('limit',new DefaultValuePipe(10),ParseIntPipe) limit?: number,
         @Query('page', new DefaultValuePipe(1), ParseIntPipe) page?: number,
     ){
@@ -19,8 +21,11 @@ export class UsersController {
 
         @Post()
         public createUser(
-            @Body(new ValidationPipe()) createUserDto: CreateUserDto){
+            @Body() createUserDto: CreateUserDto){
                 return "Create user";
             }
+
+            @Patch('/:id')
+            public updateUser(@Body() patchUserDto: PatchUserDto){}
     }
 
