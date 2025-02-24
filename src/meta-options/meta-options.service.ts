310e62849334c4b11e34f6da0f4d73dc0492d2d8
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2025-02-24 12:16:23 +0300

    update

---
+++ src/meta-options/meta-options.service.ts
@@ -0,0 +1,25 @@
+
+import { Injectable } from '@nestjs/common';
+
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MetaOption } from './meta-options.entity';
+import { CreatePostMetaOptionsDto } from './dtos/create-post-meta-options.dto';
+
+@Injectable()
+export class MetaOptionsService {
+  constructor(
+    /**
+     * Injecting metaOptions repository
+     */
+    @InjectRepository(MetaOption)
+    private metaOptionsRepository: Repository<MetaOption>,
+  ) {}
+
+  public async create(createPostMetaOptionsDto: CreatePostMetaOptionsDto) {
+    let metaOption = this.metaOptionsRepository.create(
+      createPostMetaOptionsDto,
+    );
+    return await this.metaOptionsRepository.save(metaOption);
+  }
+}
